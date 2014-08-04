a60b584d4e04549427cb488208973e88f50b0f6f
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-08-04 14:31:40 +0800

    Fixed nthRoot precision issue.

--- nthRoot.java
+++ nthRoot.java
@@ -2,6 +2,7 @@ package nthRoot;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 
 /*
 	NOTE: Always compile with '-d .' 
@@ -9,54 +10,57 @@ import java.math.BigInteger;
 */
 public class nthRoot{
 	private static final String DEFAULT_EXPONENT = "2";
+    private static final String DEFAULT_GUESS_FACTOR = "4";
+    private static final int DEFAULT_PRECISION = 2;
 	
 	public static void main( String... parameterList ){
 		String value = parameterList[ 0 ];
 
 		String exponent = DEFAULT_EXPONENT;
-
-		if( parameterList.length == 2 ){
-			try{
-				exponent = parameterList[ 1 ];
-			}catch( Exception exception ){
-				System.err.print( exception.getMessage( ) );
-				return;
-			}
+		if( parameterList.length >= 2 ){
+            exponent = parameterList[ 1 ];
 		}
 
-		BigDecimal root = nthRoot( value, exponent );
+        String guessFactor = DEFAULT_GUESS_FACTOR;
+        if( parameterList.length >= 3 ){
+            guessFactor = parameterList[ 2 ];
+        }
+
+        int precision = DEFAULT_PRECISION;
+        if( parameterList.length == 4 ){
+            try{
+                precision = Integer.parseInt( parameterList[ 3 ] );
+            }catch( Exception exception ){
+                System.err.print( exception.getMessage( ) );
+                return;
+            }
+        }
+
+		BigDecimal root = nthRoot( value, exponent, guessFactor, precision );
 		System.out.print( root.toString( ) );
 	}
 
-	public static final BigDecimal nthRoot( String value, String exponent ){
+	public static final BigDecimal nthRoot( String value, String exponent, String guessFactor, int precision ){
 		BigDecimal rootExponent = new BigDecimal( exponent );
 		BigDecimal baseValue = new BigDecimal( value );
-		BigDecimal guessRoot = baseValue.divide( new BigDecimal( "2" ) );
-		
-		BigDecimal previousGuessRoot = BigDecimal.ZERO;
-		do{
-			previousGuessRoot = guessRoot;
+		BigDecimal guessRoot = baseValue.divide( new BigDecimal( guessFactor ) );
 
+        int basePrecision = 1;
+        if( baseValue.toString( ).split( "\\." ).length > 1 ){
+            basePrecision = baseValue.toString( ).split( "\\." )[ 1 ].length( );
+        }
+
+		do{
 			BigDecimal phaseA = rootExponent.subtract( BigDecimal.ONE ).multiply( guessRoot );
 
-			System.out.println( "phase A: " + phaseA.toString( ) );
+			BigDecimal phaseB = baseValue.divide( guessRoot.pow( rootExponent.subtract( BigDecimal.ONE ).intValue( ) ), precision, RoundingMode.HALF_UP );
 
-			BigDecimal phaseB = baseValue.divide( guessRoot.pow( rootExponent.subtract( BigDecimal.ONE ).intValue( ) ) );
+			BigDecimal phaseC = BigDecimal.ONE.divide( rootExponent, precision, RoundingMode.HALF_UP );
 
-			System.out.println( "phase B: " + phaseB.toString( ) );
+			guessRoot = phaseC.multiply( phaseA.add( phaseB ) ).setScale( precision++, RoundingMode.HALF_UP );
 
-			BigDecimal phaseC = BigDecimal.ONE.divide( rootExponent );
+		}while( guessRoot.pow( rootExponent.intValue( ) ).setScale( basePrecision, RoundingMode.HALF_UP ).compareTo( baseValue ) != 0 );
 
-			System.out.println( "phase C: " + phaseC.toString( ) );
-
-			guessRoot = phaseC.multiply( phaseA.add( phaseB ) );
-
-			System.out.println( "Guess root: " + guessRoot.toString( ) );
-			System.out.println( "Previous guess root: " + guessRoot.toString( ) );
-			System.out.println( "Base value: " + baseValue.toString( ) );
-
-		}while( guessRoot.pow( rootExponent.intValue( ) ).compareTo( baseValue ) != 0 );
-		
-		return guessRoot;		
+		return guessRoot;
 	}
 }
