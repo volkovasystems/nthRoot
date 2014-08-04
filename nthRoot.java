b9cc2eb881fc6535c68188abe181ded50788590b
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-08-04 23:48:02 +0800

    Added documentation.

--- nthRoot.java
+++ nthRoot.java
@@ -37,6 +37,7 @@ public class nthRoot{
         }
 
 		BigDecimal root = nthRoot( value, exponent, guessFactor, precision );
+
 		System.out.print( root.toString( ) );
 	}
 
@@ -45,11 +46,26 @@ public class nthRoot{
 		BigDecimal baseValue = new BigDecimal( value );
 		BigDecimal guessRoot = baseValue.divide( new BigDecimal( guessFactor ) );
 
+		/*
+			The base precision is the length of numbers after the decimal point.
+			This will be used to validate the guess root.
+		*/
         int basePrecision = 1;
         if( baseValue.toString( ).split( "\\." ).length > 1 ){
             basePrecision = baseValue.toString( ).split( "\\." )[ 1 ].length( );
         }
 
+        /*
+        	This is based on Newton's Iteration Method
+
+        		guessRoot_index = ( 1 / exponent ) * ( ( ( exponent - 1 ) * guessRoot_index ) + ( baseValue / ( guessRoot_index ^ exponent - 1 ) ) )
+
+        	Phase A involves this equation: ( ( exponent - 1 ) * guessRoot_index )
+        	Phase B involves this equation: ( baseValue / ( guessRoot_index ^ exponent - 1 ) )
+        	Phase C involves this equation: ( 1 / exponent )
+
+        	guessRoot at the specific index is therefore equal to C * ( A + B )
+        */
 		do{
 			BigDecimal phaseA = rootExponent.subtract( BigDecimal.ONE ).multiply( guessRoot );
 
@@ -57,7 +73,11 @@ public class nthRoot{
 
 			BigDecimal phaseC = BigDecimal.ONE.divide( rootExponent, precision, RoundingMode.HALF_UP );
 
-			guessRoot = phaseC.multiply( phaseA.add( phaseB ) ).setScale( precision++, RoundingMode.HALF_UP );
+			guessRoot = phaseC.multiply( phaseA.add( phaseB ) ).setScale( precision, RoundingMode.HALF_UP );
+
+			//We increase the precision to match the evaluation.
+			//NOTE: Increasing the precision means increasing the number of digits after the dot.
+			precision++;
 
 		}while( guessRoot.pow( rootExponent.intValue( ) ).setScale( basePrecision, RoundingMode.HALF_UP ).compareTo( baseValue ) != 0 );
 
