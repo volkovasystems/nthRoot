11842fc9a6388506864360c5bb9e855ca8549318
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-08-12 01:24:24 +0800

    Replaced 4 spaces to hard tabs.

--- nthRoot.java
+++ nthRoot.java
@@ -12,45 +12,45 @@ import static nthPower.nthPower.nthPower;
 */
 public class nthRoot{
 	private static final String DEFAULT_EXPONENT = "2";
-    private static final String DEFAULT_GUESS_FACTOR = "4";
-    private static final int DEFAULT_PRECISION = 2;
+	private static final String DEFAULT_GUESS_FACTOR = "4";
+	private static final int DEFAULT_PRECISION = 2;
 	
 	public static void main( String... parameterList ){
 		String value = parameterList[ 0 ];
 
 		String exponent = DEFAULT_EXPONENT;
 		if( parameterList.length >= 2 ){
-            exponent = parameterList[ 1 ];
+			exponent = parameterList[ 1 ];
 		}
 
-        String guessFactor = DEFAULT_GUESS_FACTOR;
-        if( parameterList.length >= 3 ){
-            guessFactor = parameterList[ 2 ];
-        }
+		String guessFactor = DEFAULT_GUESS_FACTOR;
+		if( parameterList.length >= 3 ){
+			guessFactor = parameterList[ 2 ];
+		}
 
-        int precision = DEFAULT_PRECISION;
-        if( parameterList.length == 4 ){
-            try{
-                precision = Integer.parseInt( parameterList[ 3 ] );
-            }catch( Exception exception ){
-                System.err.print( exception.getMessage( ) );
-                return;
-            }
-        }
+		int precision = DEFAULT_PRECISION;
+		if( parameterList.length == 4 ){
+			try{
+				precision = Integer.parseInt( parameterList[ 3 ] );
+			}catch( Exception exception ){
+				System.err.print( exception.getMessage( ) );
+				return;
+			}
+		}
 
-        try{
-            BigDecimal root = nthRoot( value, exponent, guessFactor, precision );
+		try{
+			BigDecimal root = nthRoot( value, exponent, guessFactor, precision );
 
-            System.out.print( root.toString( ) );
+			System.out.print( root.toString( ) );
 
-        }catch( Exception exception ){
-            System.err.print( exception.getMessage( ) );
-        }
+		}catch( Exception exception ){
+			System.err.print( exception.getMessage( ) );
+		}
 	}
 
 	public static final BigDecimal nthRoot( String value, String exponent, String guessFactor, int precision )
-        throws Exception
-    {
+		throws Exception
+	{
 		BigDecimal rootExponent = new BigDecimal( exponent );
 		BigDecimal baseValue = new BigDecimal( value );
 		BigDecimal guessRoot = baseValue.divide( new BigDecimal( guessFactor ) );
@@ -59,28 +59,28 @@ public class nthRoot{
 			The base precision is the length of numbers after the decimal point.
 			This will be used to validate the guess root.
 		*/
-        int basePrecision = 1;
-        String[ ] baseValuePartList = baseValue.toString( ).split( "\\." );
-        if( baseValuePartList.length > 1 ){
-            basePrecision = baseValuePartList[ 1 ].length( );
-        }
+		int basePrecision = 1;
+		String[ ] baseValuePartList = baseValue.toString( ).split( "\\." );
+		if( baseValuePartList.length > 1 ){
+			basePrecision = baseValuePartList[ 1 ].length( );
+		}
 
-        /*
-        	This is based on Newton's Iteration Method
+		/*
+			This is based on Newton's Iteration Method
 
-        		guessRoot_index = ( 1 / exponent ) * ( ( ( exponent - 1 ) * guessRoot_index ) + ( baseValue / ( guessRoot_index ^ exponent - 1 ) ) )
+				guessRoot_index = ( 1 / exponent ) * ( ( ( exponent - 1 ) * guessRoot_index ) + ( baseValue / ( guessRoot_index ^ exponent - 1 ) ) )
 
-        	Phase A involves this equation: ( ( exponent - 1 ) * guessRoot_index )
-        	Phase B involves this equation: ( guessRoot_index ^ exponent - 1 )
-        	Phase C involves this equation: ( baseValue / phaseB )
-        	Phase D involves this equation: ( 1 / exponent )
+			Phase A involves this equation: ( ( exponent - 1 ) * guessRoot_index )
+			Phase B involves this equation: ( guessRoot_index ^ exponent - 1 )
+			Phase C involves this equation: ( baseValue / phaseB )
+			Phase D involves this equation: ( 1 / exponent )
 
-        	guessRoot at the specific index is therefore equal to C * ( A + B )
-        */
+			guessRoot at the specific index is therefore equal to C * ( A + B )
+		*/
 		do{
 			BigDecimal phaseA = rootExponent.subtract( BigDecimal.ONE ).multiply( guessRoot );
 
-            BigDecimal phaseB = nthPower( guessRoot.toString( ), rootExponent.subtract( BigDecimal.ONE ).toString( ) );
+			BigDecimal phaseB = nthPower( guessRoot.toString( ), rootExponent.subtract( BigDecimal.ONE ).toString( ) );
 
 			BigDecimal phaseC = baseValue.divide( phaseB, precision, RoundingMode.HALF_UP );
 
